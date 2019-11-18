f544c78ae3260eac33b62b15500a7ad38f88684d
jeffchee <jeffrey.chee@yahoo.com>
2019-11-18 13:10:36 -0800

    Updates

---
+++ precache-manifest.30d36af556ed9357edb44fbba8b2bfa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f9fe30fb40350daf97a81d75b43c032",
+    "url": "/jeffchee/simplemath.git/index.html"
+  },
+  {
+    "revision": "6bb7a6ba134f45e27202",
+    "url": "/jeffchee/simplemath.git/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bf02df0b2bed57e150a0",
+    "url": "/jeffchee/simplemath.git/static/js/2.fe4bcfe6.chunk.js"
+  },
+  {
+    "revision": "6bb7a6ba134f45e27202",
+    "url": "/jeffchee/simplemath.git/static/js/main.128ab516.chunk.js"
+  },
+  {
+    "revision": "83bfc97497a7bc8e4ef9",
+    "url": "/jeffchee/simplemath.git/static/js/runtime-main.1eaf486e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/jeffchee/simplemath.git/static/media/logo.25bf045c.svg"
+  }
+]);
